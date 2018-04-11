e106e53aa643fbe92fa49a9c056be9a77454131d
aleperalta <alejandraperaltaescamilla@gmail.com>
2018-04-10 22:14:53 -0500

    Insercion de casos de prueba
    Datos de prueba y comprobación con función.

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -10,11 +10,13 @@ Funcion que calcula la cantidad a pagar por concepto de estacionamiento
 float pago_estacionamiento(int horas)
 {
 	int cant(0);
+	if(horas == 0)
+	    cant = 0;
 	if (horas>=0 && horas<=5)
 		cant = horas*10;
 	if (horas>=6 && horas<=10)
 		cant = horas*9;
-	if (horas>11)
+	if (horas>10)
 		cant = horas*7;
 
 	return cant;
@@ -23,9 +25,20 @@ float pago_estacionamiento(int horas)
 void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
-    // Agrega tus casos de prueba
+    cout << endl << "------- EP -------" << endl;
+    cout << "Test Data | Expected Result | Actual Result | Pass/Fail" << endl;
+    cout << 3 << "         |       " << 30 << "        |   " << pago_estacionamiento(3); if(pago_estacionamiento(3) == 30) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 8 << "         |       " << 72 << "        |   " << pago_estacionamiento(8); if(pago_estacionamiento(8) == 72) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 23 << "        |       " << 161 << "       |   " << pago_estacionamiento(23); if(pago_estacionamiento(23) == 161) cout << "         |  Pass" << endl; else cout << "  Fail" << endl;
 
-
+    cout << endl << "------- BVA -------" << endl;
+    cout << "Test Data | Expected Result | Actual Result | Pass/Fail" << endl;
+    cout << 0 << "         |       " << "Error" << "     |   " << pago_estacionamiento(0); if(pago_estacionamiento(0) == 0) cout << "           |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 1 << "         |       " << 10 << "        |   " << pago_estacionamiento(1); if(pago_estacionamiento(1) == 10) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 5 << "         |       " << 50 << "        |   " << pago_estacionamiento(5); if(pago_estacionamiento(5) == 50) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 6 << "         |       " << 54 << "        |   " << pago_estacionamiento(6); if(pago_estacionamiento(6) == 54) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 10 << "        |       " << 90 << "        |   " << pago_estacionamiento(10); if(pago_estacionamiento(10) == 90) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
+    cout << 11 << "        |       " << 77 << "        |   " << pago_estacionamiento(11); if(pago_estacionamiento(11) == 77) cout << "          |  Pass" << endl; else cout << "  Fail" << endl;
 
     cout << endl;
 
